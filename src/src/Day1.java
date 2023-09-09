c80de73cc8a837b60c26705cc5f65b04c683228d
kirillwolkow <kirillwolkow@protonmail.com>
2023-09-09 22:14:37 +0200

    add part 2, calculate top three elves

--- src/src/Day1.java
+++ src/src/Day1.java
@@ -1,5 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Day1 {
@@ -8,12 +11,14 @@ public class Day1 {
         Scanner scanner = new Scanner(file);
         int maxNumber = 0;
         int currentMax = 0;
+        List<Integer> calorieList = new ArrayList<>();
 
         while (scanner.hasNextLine()) {
             String number = scanner.nextLine();
             if (!number.isEmpty()) {
                 int currentNumber = Integer.parseInt(number);
                 currentMax += currentNumber;
+                calorieList.add(currentMax);
             } else {
                 if (currentMax > maxNumber) {
                     maxNumber = currentMax;
@@ -22,5 +27,7 @@ public class Day1 {
             }
         }
         System.out.println("MAX NUMBER: " + maxNumber);
+        calorieList.sort(Collections.reverseOrder());
+        System.out.println("SUM TOP 3 ELVES: " + (calorieList.get(0) + calorieList.get(1) + calorieList.get(2)));
     }
 }
