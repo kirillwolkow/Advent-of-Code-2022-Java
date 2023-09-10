8d96865955bbd5ac29f697fe79d5901bbde1a5d0
kirillwolkow <kirillwolkow@protonmail.com>
2023-09-10 17:02:35 +0200

    add method to calculate second part of challenge

--- src/src/Day2.java
+++ src/src/Day2.java
@@ -1,20 +1,23 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Day2 {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("C:\\Users\\kenny\\programming\\java_aoc\\inputs\\day2.txt");
         Scanner scanner = new Scanner(file);
         int totalScore = 0;
+        int calculatedResultScore = 0;
 
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             char opponent = line.charAt(0);
             char choice = line.charAt(2);
             totalScore += calculateScore(opponent, choice);
+            calculatedResultScore += calculateResultScore(opponent, choice);
         }
         System.out.println("Total Score: " + totalScore);
+        System.out.println("Calculated Score: " + calculatedResultScore);
     }
 
     private static int calculateScore(char opponent, char choice) {
@@ -31,6 +34,26 @@ public class Day2 {
         } else if (choice == 'Y') {
             score += 2;
         } else if (choice == 'Z') {
+            score += 3;
+        }
+
+        return score;
+    }
+
+    private static int calculateResultScore(char opponent, char choice) {
+        int score = 0;
+
+        if (opponent == 'A' && choice == 'Y' || opponent == 'B' && choice == 'Y' || opponent == 'C' && choice == 'Y') {
+            score += 3;
+        } else if (opponent == 'A' && choice == 'Z' || opponent == 'B' && choice == 'Z' || opponent == 'C' && choice == 'Z') {
+            score += 6;
+        }
+
+        if (opponent == 'A' && choice == 'Y' || opponent == 'B' && choice == 'X' || opponent == 'C' && choice == 'Z') {
+            score += 1;
+        } else if (opponent == 'A' && choice == 'Z' || opponent == 'B' && choice == 'Y' || opponent == 'C' && choice == 'X') {
+            score += 2;
+        } else {
             score += 3;
         }
 
